49f49795d364fba62efc64d1a4f33ff95593fd28
JunHyeokDev <fixme1537@gmail.com>
2024-04-26 02:33:38 +0000

    Init Almost(?) done!

--- mm.c
+++ mm.c
@@ -24,9 +24,9 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "ateam",
+    "TeamA",
     /* First member's full name */
-    "Harry Bovik",
+    "JunHyeok,Kim",
     /* First member's email address */
     "bovik@cs.cmu.edu",
     /* Second member's full name (leave blank if none) */
@@ -35,8 +35,34 @@ team_t team = {
     ""
 };
 
+static char* heap_listp;
+
+
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
+#define WSIZE 4 // word and header size (bytes)
+#define DSIZE 8 // Double word size 
+#define CHUNKSIZE (1<<12) // Extend heap by this amount when it is needed (bytes)!
+
+/* 메모리 블록의 크기(Size)와 할당 여부(alloc)를 비트 단위로 조합한 값입니다.  */
+/* combines a size and an allocate bit and returns a value that can be stored in a header or footer.*/
+#define PACK(size,alloc) ((size) | (alloc)) 
+
+/* The GET reads and returns the word referenced by argu- ment p.*/
+/* The casting here is crucial. The argument p is typically a (void *) pointer, which cannot be dereferenced directly. */
+/* p를 (unsigned int *) 포인터 타입으로 캐스팅 한 뒤 값을 역참조 하겠다는 뜻 입니다! */
+#define GET(p) (*(unsigned int *)(p)) 
+/* 단순히 해당 위치의 값을 val로 넣는 것 입니다! */
+#define PUT(p,val) (*(unsigned int *)(p) = (val)) 
+
+#define GET_SIZE(p)  (GET(p) & ~0x7) /* p 10진수 크기 값을 반환합니다*/ 
+#define GET_ALLOC(p) (GET(p) & 0x1)  /* p 의 할당 여부를 반환합니다*/ 
+
+#define HDRP(bp) ((char *)(bp) - WSIZE) /* HDRP 는 Header Pointer의 약자입니다 */
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE) /* HDRP 는 Header Pointer의 약자입니다 */
+
+#define NEXT_BLKP(bp) ((char*)(bp) + GET_SIZE(((char*)(bp)-WSIZE)))
+#define PREV_BLKP(bp) ((char*)(bp) + GET_SIZE(((char*)(bp)-DSIZE)))
 
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
@@ -47,8 +73,45 @@ team_t team = {
 /* 
  * mm_init - initialize the malloc package.
  */
+
+static void* extend_heap(size_t words) {
+    char *bp;
+    size_t size;
+
+    /* 짝수면 words % 2 가 홀수가 나오겟지? 그러므로 words+1이 실행됨 */
+    size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
+    if ((long) (bp = (char*)mem_sbrk(size)) == -1) { // bp할당 
+        return NULL;
+    }
+    PUT(HDRP(bp), PACK(size,0));  // 해당 위치 Header Pointer 값 설정
+    PUT(FTRP(bp), PACK(size,0));  // 해당 위치 Footer Pointer 값 설정
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0,1)); // 에필로그는 크기가 0이라고 했지.
+    // printf("HDRP : %p\n", HDRP(bp));
+    // printf("FTRP : %p\n", FTRP(bp));
+    // printf("Epil : %p\n", HDRP(NEXT_BLKP(bp)));
+
+    return NULL;
+}
+
 int mm_init(void)
 {
+
+    if ((heap_listp = mem_sbrk(4*WSIZE)) == (void*) -1 ) {
+        return -1;
+    }
+    PUT(heap_listp,0); // Alignment padding
+    PUT(heap_listp + (1*WSIZE),  PACK(DSIZE,1)); // Prologue header
+    PUT(heap_listp + (2*WSIZE),  PACK(DSIZE,1)); // Prologue footer
+    PUT(heap_listp + (3*WSIZE),  PACK(0,1));     // Epilogue header
+    printf("start !!!!\n");
+    printf("heap_listp  : %p\n",heap_listp);
+    printf("HDRP : %p\n", heap_listp + (1*WSIZE));
+    printf("FTRP : %p\n", heap_listp + (2*WSIZE));
+    printf("Epil : %p\n", heap_listp + (3*WSIZE));
+    heap_listp += (2*WSIZE);
+    printf("heap_listp  : %p\n",heap_listp);
+    printf("end !!!!\n");
+    extend_heap(CHUNKSIZE/WSIZE);
     return 0;
 }
 
